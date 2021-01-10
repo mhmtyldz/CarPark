cc65eea6608441fecf8eeac5b351217f7aae3c4d
mhmtyldz <yldzmahmut0@gmail.com>
2021-01-10 14:48:41 +0300

    Add project files.

---
+++ CarPark.Business/test.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarPark.Business
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string NameSurname { get; set; }
+        public string Description { get; set; }
+    }
+}
